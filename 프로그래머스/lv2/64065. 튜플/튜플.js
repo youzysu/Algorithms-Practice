849fdfd7400cdf7f67c020a1bdb71266cdd9dbb4
Jisoo Yoo <youzysu@gmail.com>
2023-05-13 18:11:45 +0900

    [level 2] Title: 튜플, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.js
@@ -0,0 +1,15 @@
+// 모든 집합이 가지고 있는 숫자가 배열의 첫번째 요소
+// 집합에서 가지고 있는 개수에 따라 내림차순으로 결정된다.
+
+function solution(s) {
+    const obj = {}
+    const regex = /\d+/g
+    const t = s.match(regex)
+    t.forEach((num) => {
+        obj[num] = obj[num] ? obj[num] + 1 : 1
+    })
+
+    const entries = Object.entries(obj)
+    entries.sort((a, b) => b[1] - a[1])
+    return entries.map((item) => Number(item[0]))
+}
